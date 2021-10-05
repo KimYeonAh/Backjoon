6348214697fb4eb8ae0220be724b99528dddde1a
KimYeonAh <50138845+KimYeonAh@users.noreply.github.com>
2021-10-05 23:58:54 +0900

    토마토

--- python/Boj_7576.py
+++ python/Boj_7576.py
@@ -1,4 +1,5 @@
-import queue
+from collections import deque
+
 M, N = map(int, input().split())
 
 graph = []
@@ -11,26 +12,26 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
 def bfs() :
-    x, y = que.get()
+    x, y = que.popleft()
     visited[x][y] = 1
     for i in range(4) :
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < N and 0 <= ny < M :
             if graph[nx][ny] == 0 and visited[nx][ny] == 0:
-                que.put([nx, ny])
+                que.append([nx, ny])
                 graph[nx][ny] = graph[x][y] + 1
 
     return graph
 
-que = queue.Queue()
+que = deque()
 
 for i in range(N) :
     for j in range(M) :
         if graph[i][j] == 1:
-            que.put([i, j])
+            que.append([i, j])
 
-while not que.empty() :
+while que :
     bfs()
 
 count = 0
