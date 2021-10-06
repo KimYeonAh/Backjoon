09292170a1353b7da36fd350b0183b4fc5adb1ea
KimYeonAh <50138845+KimYeonAh@users.noreply.github.com>
2021-10-06 16:28:19 +0900

    미로 탐색

---
+++ python/Boj_2178.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+N, M = map(int, input().split())
+
+graph = []
+visited = [[0]*M for _ in range(N)]
+
+for i in range(N):
+    graph.append(list(map(int, input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+queue = deque()
+queue.append([0, 0])
+
+def bfs() :
+    x, y = queue.popleft()
+    visited[x][y] = 1
+    for i in range(4) :
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M :
+            if graph[nx][ny] == 1 and visited[nx][ny] == 0 :
+                queue.append([nx, ny])
+                graph[nx][ny] = graph[x][y] + 1
+
+    return graph
+                    
+while queue :
+    bfs()
+
+print(graph[N-1][M-1])
