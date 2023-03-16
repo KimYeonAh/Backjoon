d89cd8c7c37fda8059f88602427e39662da59cef
KimYeonAh <50138845+KimYeonAh@users.noreply.github.com>
2023-03-16 16:41:58 +0900

    회의실 배정

--- Java/Boj_1931.java
+++ Java/Boj_1931.java
@@ -4,13 +4,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		    StringTokenizer st = new StringTokenizer(br.readLine());
-		    int N = Integer.parseInt(st.nextToken());
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	int N = Integer.parseInt(st.nextToken());
         int[][] timetable = new int[N][2];
 
         for(int i = 0;i < N;i++) {
             st = new StringTokenizer(br.readLine());
-		        timetable[i][0] = Integer.parseInt(st.nextToken());
+	    timetable[i][0] = Integer.parseInt(st.nextToken());
             timetable[i][1] = Integer.parseInt(st.nextToken());
         }
 
@@ -33,3 +33,52 @@ public class Main {
         System.out.println(count);
     }
 }
+
+/*  class 만들기
+import java.util.*;
+import java.io.*;
+
+class Meeting implements Comparable<Meeting>{
+    int start, end;
+
+    public Meeting(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+    @Override
+    public int compareTo(Meeting o) {
+        return this.end != o.end ? this.end-o.end : this.start-o.start;        
+    }
+}
+
+public class Boj_1931_회의실배정_김연아 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        
+        ArrayList<Meeting> timetable = new ArrayList<>();
+        
+        for(int i = 0;i < N;i++) {
+            st = new StringTokenizer(br.readLine());
+            timetable.add(new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        
+        }
+
+        Collections.sort(timetable);
+
+        int count = 1;
+        int start = timetable.get(0).end;
+
+        for(int i = 1; i < timetable.size(); i++){
+            if (start <= timetable.get(i).start){
+                start = timetable.get(i).end;
+                count ++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+*/
